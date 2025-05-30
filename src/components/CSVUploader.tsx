34fce0109336ea50fa57c239d7c76ed182286939
Mihaela Hristova <m.hristova90@gmail.com>
2025-05-30 11:31:11 +0300

    fix: remove mock data

--- src/components/CSVUploader.tsx
+++ src/components/CSVUploader.tsx
@@ -11,18 +11,11 @@ type CSVUploaderProps = {
 
 export default function CSVUploader({ onParsed }: CSVUploaderProps) {
 
-  const mockCSV = `
-1,1001,2020-01-01,2020-03-01
-2,1001,2020-02-01,2020-04-01
-3,1002,01/03/2020,01/06/2020
-4,1002,2020/04/01,null
-5,1001,15.02.2020,15.03.2020
-`;
   const handlePickFile = async () => {
     try {
-      // const res = await DocumentPicker.pickSingle({ type: [DocumentPicker.types.plainText] });
-      // const fileContent = await fetch(res.uri).then(r => r.text());
-      const result = parseCSVAndCalculate(mockCSV);
+      const res = await DocumentPicker.pickSingle({ type: [DocumentPicker.types.plainText] });
+      const fileContent = await fetch(res.uri).then(r => r.text());
+      const result = parseCSVAndCalculate(fileContent);
       console.log('Parsed CSV Result:', result);
       onParsed(result);
     } catch (err) {
